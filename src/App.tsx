18974d7704f2f10ac997863821b6e9dfc16e401e
Christopher Sanchez <shonen787@gmail.com>
2023-05-26 20:48:51 -0400

    Moving Function Arounds

--- src/App.tsx
+++ src/App.tsx
@@ -5,29 +5,27 @@ import _cards from './assets/cards/cards.json'
 import Card from './Card'
 import Header from './Header'
 
-async function GetCardImage(card: CardObj): Promise<string>{
-  
-  const res = await fetch(`https://api.scryfall.com/cards/named?fuzzy=${card.name.trim()}`);
-  if (res.ok){
-    const image_uri = await res.json();
-    return image_uri.image_uris.border_crop || "";
-
-  } 
-
-  return ""
-}
 
 function App() {
   const cards = _cards as CardObj[];
   const [cardsData, setCardsData] = useState<CardObj[]>([]);
 
   useEffect(() => {
-    Promise.all(cards.map(async card => {
+        async function GetCardImage(card: CardObj): Promise<string>{
+      
+          const res = await fetch(`https://api.scryfall.com/cards/named?fuzzy=${card.name.trim()}`);
+          if (res.ok){
+            const image_uri = await res.json();
+            return image_uri.image_uris.border_crop || "";
+          }         
+          return ""
+        }
+      Promise.all(cards.map(async card => {  
       const image_uri = await GetCardImage(card);
       return {...card, image_uri};
     })).then(cardsData => setCardsData(cardsData));
-  },[]);
-
+  }
+  ,[]);
 
 
   return (
